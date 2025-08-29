4c1acb40a62eb0ef6cebe5dd50a77c9818b399c2
ahmadrifai <ahmatrifai8721@gmail.com>
2025-08-29 11:43:53 +0700

    update api record

--- routes/api.php
+++ routes/api.php
@@ -35,7 +35,9 @@ Route::put('/log-entry/import-prtg', function (Request $request) {
     Log::info($data);
 
     $logEntry = \App\Models\LogEntry::updateOrCreate([
+        'lastdown'    => $data['lastdown'],
         'deviceid'    => $data['deviceid'],
+        'status'      => $data['laststatus'],
     ], [
         'client_name' => $data['device'] ?? 'Tidak diketahui',
         'ip_address'  => $data['host'] ?? '0.0.0.0',
