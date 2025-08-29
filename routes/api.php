b9fd90c39b94240bf91dd8a3d2e6b75d76a093cb
ahmadrifai <ahmatrifai8721@gmail.com>
2025-08-29 11:06:35 +0700

    update json parse uncoment

--- routes/api.php
+++ routes/api.php
@@ -20,38 +20,38 @@ Route::get('/log-entry/import-prtg', function (Request $request) {
     $jsonContent = urldecode($jsonContent);
     $data = json_decode($jsonContent, true);
 
-    // $timestamp = !empty($data['%lastcheck'])
-    //     ? \Carbon\Carbon::createFromFormat('d/m/Y H:i:s', $data['%lastcheck'])->format('Y-m-d H:i:s')
-    //     : now();
-    // return response()->json([
-    //     'success' => false,
-    //     'message' => 'Data PRTG gagal diimpor.',
-    //     'data' => $data
-    // ], 200);
+    $timestamp = !empty($data['%lastcheck'])
+        ? \Carbon\Carbon::createFromFormat('d/m/Y H:i:s', $data['%lastcheck'])->format('Y-m-d H:i:s')
+        : now();
+    return response()->json([
+        'success' => false,
+        'message' => 'Data PRTG gagal diimpor.',
+        'data' => $data
+    ], 200);
 
-    dump($jsonContent);
-    dd($data);
-    // Log::info($jsonContent);
-    // Log::info($data);
+    // dump($jsonContent);
+    // dd($data);
+    Log::info($jsonContent);
+    Log::info($data);
 
-    // $logEntry = \App\Models\LogEntry::updateOrCreate([
-    //     'ip_address'  => $data['host'] ?? '0.0.0.0',
-    //     'lastdown'    => $data['lastdown'] ?? null,
-    // ], [
-    //     'client_name' => $data['device'] ?? 'Tidak diketahui',
-    //     'ip_address'  => $data['host'] ?? '0.0.0.0',
-    //     'status'      => $data['laststatus'] ?? 'Unknown',
-    //     'root_cause'  => $data['history'] ?? ($data['group'] ?? 'Lainnya'),
-    //     'lastdown'    => $data['lastdown'] ?? null,
-    //     'deviceid'    => $data['deviceid'] ?? null,
-    //     'downtime'    => $data['downtime'] ?? null,
-    //     'timestamp'   => $timestamp,
-    // ]);
-    // // Log::info('log enttri:', $logEntry->toArray());
+    $logEntry = \App\Models\LogEntry::updateOrCreate([
+        'ip_address'  => $data['host'] ?? '0.0.0.0',
+        'lastdown'    => $data['lastdown'] ?? null,
+    ], [
+        'client_name' => $data['device'] ?? 'Tidak diketahui',
+        'ip_address'  => $data['host'] ?? '0.0.0.0',
+        'status'      => $data['laststatus'] ?? 'Unknown',
+        'root_cause'  => $data['history'] ?? ($data['group'] ?? 'Lainnya'),
+        'lastdown'    => $data['lastdown'] ?? null,
+        'deviceid'    => $data['deviceid'] ?? null,
+        'downtime'    => $data['downtime'] ?? null,
+        'timestamp'   => $timestamp,
+    ]);
+    // Log::info('log enttri:', $logEntry->toArray());
 
-    // return response()->json([
-    //     'success' => true,
-    //     'message' => 'Data PRTG berhasil diimpor.',
-    //     'data' => $logEntry
-    // ]);
+    return response()->json([
+        'success' => true,
+        'message' => 'Data PRTG berhasil diimpor.',
+        'data' => $logEntry
+    ]);
 });
